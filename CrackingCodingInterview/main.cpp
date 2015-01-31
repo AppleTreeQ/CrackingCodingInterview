3c3a5c843f0ae2f5f170f89016aae9665136ce8f
Qian Zhai <zhaiqianzq@gmail.com>
2015-01-31 15:42:07 +0900

    add

---
+++ CrackingCodingInterview/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CrackingCodingInterview
+//
+//  Created by ipuser on 2015/01/29.
+//  Copyright (c) 2015年 ipuser. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
